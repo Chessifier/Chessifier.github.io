18f7412866db37c5a6ad334290ecfb77036d1ccd
Rustam Abrahamyan <rustam.wd@gmail.com>
2025-08-07 15:21:06 +0400

    chore: add screenshots and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,43 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-    site: 'https://chessifier.github.io',
-    base: '/',
-    integrations: [
-        starlight({
-            title: 'Chessifier',
-            social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Chessifier/Chessifier' }],
-            sidebar: [
-                {
-                    label: 'Installation Guide',
-                    slug: 'installation-guide'
-                },
-            ],
-            components: {
-                Header: './src/components/Header.astro',
-            },
-        }),
-    ],
+  site: 'https://chessifier.github.io',
+  base: '/',
+  integrations: [
+    starlight({
+      title: 'Chessifier',
+      description: 'Chessifier Documentation – Learn how to install and use Chessifier.',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/Chessifier/Chessifier',
+        },
+      ],
+      favicon: '/favicon.ico',
+      head: [
+        {
+          tag: 'meta',
+          attrs: { name: 'author', content: 'Chessifier Team' },
+        },
+        {
+          tag: 'link',
+          attrs: { rel: 'canonical', href: 'https://chessifier.github.io/Chessifier/' },
+        },
+      ],
+      sidebar: [
+        {
+          label: 'Installation Guide',
+          slug: 'installation-guide',
+        },
+      ],
+      components: {
+        Header: './src/components/Header.astro',
+      },
+    }),
+    sitemap(),
+  ],
 });
